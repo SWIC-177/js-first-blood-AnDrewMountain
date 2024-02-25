be7e246b85733a6cb9a6924706affcade7edec74
AnDrewMountain <andrewjmountain@gmail.com>
2024-02-24 18:39:06 -0600

    Function greet commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define the function
+function greet(name) {
+    return name 
+}
+
+// Call the function 
+//var greeting = greet(Andrew);
+
+//Print result in the console
+//console.log("Hello, ", name);
