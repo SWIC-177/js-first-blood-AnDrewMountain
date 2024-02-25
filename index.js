0658a69669fb379bbfed490baae04d122e5fa3f9
AnDrewMountain <andrewjmountain@gmail.com>
2024-02-24 19:55:53 -0600

    Fixing error on name being undefined

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Define the function
-const greet = (name) => {
-    return `Hello, ${name}!`;
+function greet(name) {
+    return name;
 }
+/*const greet = (name) => {
+    return `Hello, ${name}!`;
+}*/
 /*function greet(name) {
     return '${name}!' 
 }*/
 
 // Call the function 
-var greeting = greet(Andrew);
+const myName = "Andrew";
+const greeting = greet(myName);
 
 //Print result in the console
-console.log("Hello, ", name);
+console.log("Hello, ",greeting);
