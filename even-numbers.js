3e68305378bb81dd4c7cc94eb1d79c86b76bb71d
AnDrewMountain <andrewjmountain@gmail.com>
2024-03-28 21:36:51 -0500

    even-numbers.js and fizzbuzz added

---
+++ even-numbers.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Loop through the array
+for (let i = 0; i < numbers.length; i++) {
+    // Check if the number is even
+    if (numbers[i] % 2 === 0) {
+        // Log the even number
+        console.log(numbers[i]);
+    }
+}
---
+++ fizzbuzz.js
@@ -0,0 +1,27 @@
+// Using a while loop
+let num = 1;
+while (num <= 100) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+    num++;
+}
+
+// Using a for loop
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
